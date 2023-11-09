905aebad3b7a6ea5320f96a7e26e478ffc6707fa
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-11-08 20:02:18 -0600

    finish assignment

--- queries.sql
+++ queries.sql
@@ -8,7 +8,10 @@ WHERE location = "St. Louis City";
 --Part 3
 DROP TABLE job;
 --Part 4
-SELECT skill
-FROM job
+SELECT *
+FROM SKILL
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.job_id IS NOT NULL
+ORDER BY name ASC;
 
 
